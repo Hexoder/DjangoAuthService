3144ded89563a1b4130fd0c174361f4785615417
Armin Fekri <armiin.fekri1@gmail.com>
2025-04-29 14:25:48 +0330

    changed to secure

--- auth_service/grpc_client/client.py
+++ auth_service/grpc_client/client.py
@@ -64,8 +64,8 @@ class AuthClient:
             if cls._instance is None:
                 cls._instance = super(AuthClient, cls).__new__(cls)
 
-                # cls._instance.channel = get_secure_channel(server_address)
-                cls._instance.channel = grpc.insecure_channel(cls._conn_address)
+                cls._instance.channel = get_secure_channel(server_address)
+                # cls._instance.channel = grpc.insecure_channel(cls._conn_address)
 
                 cls._instance.stub = auth_pb2_grpc.AuthServiceStub(cls._instance.channel)
 
