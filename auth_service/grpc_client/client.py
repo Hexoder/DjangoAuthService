07e97014ba2901a94450b7cc3099dce611960485
imansmd <i.sadeghian1999@gmail.com>
2025-05-01 19:18:52 +0330

    'fix : cert path'

--- auth_service/grpc_client/client.py
+++ auth_service/grpc_client/client.py
@@ -10,7 +10,7 @@ import atexit
 
 def get_secure_channel(server_domain):
 
-    if hasattr(settings.AUTH_CERT_FILE_PATH):
+    if hasattr(settings ,'AUTH_CERT_FILE_PATH'):
         cert_path = Path(settings.AUTH_CERT_FILE_PATH)
     if not cert_path.exists():
         cert_path = 'authservice.pem'
