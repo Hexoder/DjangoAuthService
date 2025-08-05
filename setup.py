41e9685864a77f24352a1f04d91c392693b5e684
Armin Fekri <armiin.fekri1@gmail.com>
2025-08-05 15:59:39 +0330

    updated proto to support pname, description for SubServiceInfo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auth_package",
-    version="1.4.4.3",
+    version="1.4.4.4",
     packages=find_packages(),
     install_requires=[
         "requests",
